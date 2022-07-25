e470a8ec525ea17c52ea45786dd04d0da1acb1fa
zerobfn <aibar.baktygaliyev@alumni.nu.edu.kz>
2022-07-26 02:50:05 +0600

    [FIXED] vue config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,4 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
+module.exports = {
     publicPath: process.env.NODE_ENV === 'production' ? '/targemy-task/' : '',
     pwa: {
         workboxOptions: {
@@ -7,4 +6,4 @@ module.exports = defineConfig({
         }
     },
     transpileDependencies: true
-})
+}
