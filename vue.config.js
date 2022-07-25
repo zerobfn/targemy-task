6385e8ead7d95e16434ce42c2d72cc922e7b7d38
zerobfn <aibar.baktygaliyev@alumni.nu.edu.kz>
2022-07-26 02:42:06 +0600

    [UPDATED] vue config

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-    publicPath: process.env.NODE_ENV === 'production' ? '/tergamy/' : '',
+    publicPath: process.env.NODE_ENV === 'production' ? '/targemy-task/' : '',
     pwa: {
         workboxOptions: {
             skipWaiting: true
